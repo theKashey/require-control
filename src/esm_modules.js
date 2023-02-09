d287fb9976b992f4b5b95c2c400285a121229aee
Anton Korzunov <thekashey@gmail.com>
2023-02-09 14:38:10 +1100

    remove comments before looking for exports, fixes: #6

--- src/esm_modules.js
+++ src/esm_modules.js
@@ -2,17 +2,23 @@ const fs = require("fs");
 const babelIt = require('./babelIt');
 
 const esm_modules = babelIt(
-  fileName => {
-    if (fileName.indexOf('node_modules') > 0) {
-      const code = fs.readFileSync(fileName);
-      // no imports inside
-      return code.indexOf('import ') !== code.indexOf('export ') && code;
+    fileName => {
+        if (fileName.indexOf('node_modules') > 0) {
+            const code = fs.readFileSync(fileName);
+            // use reduced version of the code to remove false positives of the check
+            // remove comments, but keywords can still be found in strings
+            const reducedCode = code.toString('utf-8')
+                .replace(/\/\*([^\*]*)\*\//gi, '')
+                .replace(/\/\*(.*)\*\//gi, '')
+                .replace(/\/\/(.*)/gi, '')
+            // no imports inside
+            return reducedCode.indexOf('import ') !== reducedCode.indexOf('export ') && code;
+        }
+        return false;
+    }, {
+        babelrc: false,
+        plugins: ["@babel/plugin-transform-modules-commonjs"]
     }
-    return false;
-  }, {
-    babelrc: false,
-    plugins: ["transform-es2015-modules-commonjs"]
-  }
 );
 
 module.exports = esm_modules;
